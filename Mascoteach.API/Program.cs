2a37e028a98a18d028028e1e34341b020b84ce84
Hoàng Lê Thành Đức <fptuduchoang@gmail.com>
2026-03-15 22:40:53 +0700

    cors updated

--- Mascoteach.API/Program.cs
+++ Mascoteach.API/Program.cs
@@ -78,9 +78,13 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll", policy =>
     {
         policy.WithOrigins(
-                "https://localhost:5173",    
-                "https://localhost:5500",     
+                "http://localhost:5173",     
+                "https://localhost:5173",
+                "http://localhost:5500",     
+                "https://localhost:5500",
+                "http://127.0.0.1:5173",     
                 "https://127.0.0.1:5173",
+                "http://127.0.0.1:5500",     
                 "https://127.0.0.1:5500",
                 "https://mascoteach.com"
               )
